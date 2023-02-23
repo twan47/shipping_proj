736f8d5b985f9dfd684181a5cb2073aa3ca3ef21
Twan47 <anthonynewell171@gmail.com>
2023-02-23 11:38:58 -0500

    added premium and drone shipping cost

--- shipping.py
+++ shipping.py
@@ -7,5 +7,21 @@ elif weight > 2 and weight <= 6:
     cost_ground = weight * 3.00 + 20.00
 elif weight > 6 and weight <= 10:
     cost_ground = weight * 4.00 + 20.00
-elif weight > 10:
+else:
     cost_ground = weight * 4.75 + 20.00
+print(cost_ground)
+
+#ground shipping premium
+cost_ground_premium = 125
+print(cost_ground_premium)
+
+#drone shipping
+if weight <= 2:
+    drone_shipping = weight * 4.50
+elif weight > 2 and weight <= 6:
+    drone_shipping = weight * 9.00
+elif weight > 6 and weight <= 10:
+    drone_shipping = weight * 12.00
+else:
+    drone_shipping = 14.25
+print(drone_shipping)
